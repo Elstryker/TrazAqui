b1fd3591f2a0ebb62dcc25adcb3c68f1ae0e7316
Elstryker <rubencorreiacerqueira@gmail.com>
2020-04-18 15:53:01 +0100

    basically done Loja

--- src/TrazAqui/Loja.java
+++ src/TrazAqui/Loja.java
@@ -1,58 +1,81 @@
-
 package TrazAqui;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Objects;
 import java.util.Queue;
 
 public class Loja {
-    private int codLoja;
+    private String codLoja;
     private String nome;
     private GPS localizacao;
-    private Queue<Encomenda> filaEspera;
+    private double tempoProcessamento;
+    private boolean temFila;
+    private LinkedList<Encomenda> filaEspera;
 
     public Loja () {
-        this.codLoja = 0;
+        this.codLoja = "";
         this.nome = "";
         this.localizacao = new GPS();
-        this.filaEspera = new LinkedList<Encomenda>();
+        this.tempoProcessamento = 0;
+        this.temFila = true;
+        this.filaEspera = new LinkedList<>();
     }
 
-    public Loja(int codLoja, String nome, GPS localizacao) {
+    public Loja(String codLoja, String nome, double tempoProcessamento, GPS localizacao, boolean temFila, LinkedList<Encomenda> l) {
         this.codLoja = codLoja;
         this.nome = nome;
         this.localizacao = localizacao;
+        this.tempoProcessamento = tempoProcessamento;
+        this.temFila = temFila;
+        if(temFila)
+            this.setFilaEspera(l);
+        else this.filaEspera = null;
     }
 
     public Loja(Loja a) {
         this.codLoja = a.getCodLoja();
         this.nome = a.getNome();
         this.localizacao = a.getLocalizacao();
-        this.setFilaEspera(a.getFilaEspera());
+        this.tempoProcessamento = a.getTempoProcessamento();
+        this.temFila = a.getTemFila();
+        if(this.temFila)
+            this.setFilaEspera(a.getFilaEspera());
+        else this.filaEspera = null;
+    }
+
+    public boolean getTemFila() {
+        return temFila;
+    }
+
+    public void setTemFila(boolean temFila) {
+        this.temFila = temFila;
     }
 
     public LinkedList<Encomenda> getFilaEspera() {
         LinkedList<Encomenda> s = new LinkedList<Encomenda>();
-        Iterator<Encomenda> it = s.iterator();
-        while(it.hasNext())
-            s.add(it.next().clone());
+        for (Encomenda encomenda : this.filaEspera) s.add(encomenda.clone());
         return s;
     }
 
     public void setFilaEspera(LinkedList<Encomenda> a) {
         LinkedList<Encomenda> s = new LinkedList<Encomenda>();
-        Iterator<Encomenda> it = s.iterator();
-        while(it.hasNext())
-            s.add(it.next().clone());
+        for (Encomenda encomenda : this.filaEspera) s.add(encomenda.clone());
         this.filaEspera = s;
     }
 
-    public int getCodLoja() {
+    public double getTempoProcessamento() {
+        return tempoProcessamento;
+    }
+
+    public void setTempoProcessamento(double tempoProcessamento) {
+        this.tempoProcessamento = tempoProcessamento;
+    }
+
+    public String getCodLoja() {
         return codLoja;
     }
 
-    public void setCodLoja(int codLoja) {
+    public void setCodLoja(String codLoja) {
         this.codLoja = codLoja;
     }
 
@@ -77,9 +100,11 @@ public class Loja {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Loja loja = (Loja) o;
-        return this.codLoja == loja.getCodLoja() &&
+        return this.codLoja.equals(loja.getCodLoja()) &&
                 this.nome.equals(loja.getNome()) &&
-                this.localizacao.equals(loja.getLocalizacao());
+                this.tempoProcessamento == loja.getTempoProcessamento() &&
+                this.localizacao.equals(loja.getLocalizacao()) &&
+                this.filaEspera.equals(loja.getFilaEspera());
     }
 
     @Override
@@ -100,4 +125,36 @@ public class Loja {
     public Loja clone() {
         return new Loja(this);
     }
+
+    public void adicionaEncomenda(Encomenda a) {
+        this.filaEspera.add(a);
+    }
+
+    public void removeEncomenda() {
+        if(!this.filaEspera.isEmpty())
+            this.filaEspera.remove();
+    }
+
+    public int sizeQueue() {
+        return this.filaEspera.size();
+    }
+
+    public boolean haveEncomenda(String userID) {
+        boolean ret = false;
+        for(Encomenda e: this.filaEspera)
+            if(e.getDest().getCodigo().equals(userID)) ret = true;
+        return ret;
+    }
+
+    public double tempoDeEspera(String codEnc) {
+        double ret = 0;
+        for(Encomenda e: this.filaEspera)
+            if(e.getCodigo().equals(codEnc)) {
+                int x = this.filaEspera.indexOf(e);
+                ret = (x+1) * this.tempoProcessamento;
+            }
+        return ret;
+    }
+
+
 }
---
+++ src/TrazAqui/UI.java
@@ -0,0 +1,7 @@
+package TrazAqui;
+
+public class UI {
+    public void print(String s) {
+        System.out.println(s);
+    }
+}
