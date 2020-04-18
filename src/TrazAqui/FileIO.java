de565911bc2f901172f29b802346370a41aecac8
Elstryker <rubencorreiacerqueira@gmail.com>
2020-04-18 18:04:11 +0100

    Commit 2

--- src/TrazAqui/FileIO.java
+++ src/TrazAqui/FileIO.java
@@ -1,4 +1,33 @@
 package TrazAqui;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class FileIO {
+    private ArrayList<String> lines;
+
+    public FileIO() {
+        lines = new ArrayList<>();
+    }
+
+    public void processLine() throws IOException {
+        BufferedReader file;
+        file = new BufferedReader(new FileReader("teste.txt"));
+        String line = file.readLine();
+        String[] tokens;
+        int start = 42;
+        int i = 0;
+        while(line!=null) {
+            if(i > start) {
+                tokens = line.split(":");
+                System.out.print(tokens[0]+"\n");
+                tokens = tokens[1].split(",");
+                for(String s : tokens) System.out.println(s);
+                System.out.println("Proximo elemento");
+            }
+            else i++;
+            line = file.readLine();
+        }
+        file.close();
+    }
 }
--- src/TrazAqui/Main.java
+++ src/TrazAqui/Main.java
@@ -4,12 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Dados d = new Dados();
-
-        d.leitor("teste.txt");
-        d.escritor("ola.txt");
-
-        System.out.println(d.toString());
-
+        FileIO o = new FileIO();
+        o.processLine();
     }
 }
