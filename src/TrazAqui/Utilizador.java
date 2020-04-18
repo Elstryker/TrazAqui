4261d2772d6a1ff7c72afe01fa73bc7f11ee1e19
JoelM00 <jsmartins2000@hotmail.com>
2020-04-18 13:55:41 +0100

    update

--- src/TrazAqui/Utilizador.java
+++ src/TrazAqui/Utilizador.java
@@ -1,31 +1,31 @@
 package TrazAqui;
 
-public class Ultilizador {
+public class Utilizador {
     //Variaveis de instancia
     private String nome;
     private String codigo;
     private GPS posicao;
 
-    public Ultilizador() {
+    public Utilizador() {
         this.nome = "";
         this.codigo = "";
         this.posicao = new GPS();
     }
 
-    public Ultilizador(String n,String c,GPS pos) {
+    public Utilizador(String n, String c, GPS pos) {
         this.nome = n;
         this.posicao = pos;
         this.codigo = c;
     }
 
-    public Ultilizador(Ultilizador u) {
+    public Utilizador(Utilizador u) {
         this.nome = u.getNome();
         this.codigo = u.getCodigo();
         this.posicao = u.getPosicao();
     }
 
-    public Ultilizador clone() {
-        return new Ultilizador(this);
+    public Utilizador clone() {
+        return new Utilizador(this);
     }
 
     @Override
@@ -41,7 +41,7 @@ public class Ultilizador {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o==null || this.getClass().equals(o.getClass())) return true;
-        Ultilizador u = (Ultilizador) o;
+        Utilizador u = (Utilizador) o;
 
         return this.posicao.equals(u.getPosicao()) && this.codigo.equals(u.getCodigo()) && this.nome.equals(u.getNome());
     }
