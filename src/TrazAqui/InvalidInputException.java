c8d4fa4e5756578345990c1637c29cba74cc7c58
JoelM00 <61049184+JoelM00@users.noreply.github.com>
2020-06-03 12:28:33 +0100

    Update InvalidInputException.java

--- src/TrazAqui/InvalidInputException.java
+++ src/TrazAqui/InvalidInputException.java
@@ -1,6 +1,10 @@
 package TrazAqui;
 
 public class InvalidInputException extends Exception {
+    /**
+     * Erro quando o input dado e invalido
+     * @param s String
+     */
     public InvalidInputException(String s) {
         super(s);
     }
